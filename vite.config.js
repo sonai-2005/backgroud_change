a47b116740908081e1831206bf605be1fa5161b5
sonai-2005 <nandisonai60@gmail.com>
2025-03-21 21:10:29 +0530

    fix the white page error

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base :"https://sonai-2005.github.io/backgroud_change/",
 })
